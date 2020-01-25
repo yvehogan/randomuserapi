e070e71127f09586bfbd4149a4b4c51031fefb56
yvehogan <mz.yve3@gmail.com>
2020-01-25 02:09:24 -1100

    init markup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Spinner, Card } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+useEffect(async () => {
+const response = await fetch(url);
+const data = await response.json();
+const [item] = data.results;
+setData(item);
+setLoading(false);
+}, []);
+
+return {data, loading};
 }
 
-export default App;
+export default() => {
+  const {data, loading} = useFetch('https://randomuser.me/api/')
+  
+
+return (
+  <div className='center'>
+    <p>Reload the page to get another random user</p>
+    {loading ? <div style={{fontSize: '8rem'}} ><Spinner style={{width: '8rem', height: '8rem'}} animation="grow" /></div> : 
+    <div> 
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={data.picture.large} />
+      <Card.Body style={{ backgroundColor: '#333', color: '#fff' }}>
+        <Card.Title>
+          <strong>{data.name.title}. {data.name.first} {data.name.last} </strong>
+        </Card.Title>
+        <Card.Text>
+          Gender: {data.gender} <br/> <hr/>
+          Age: {data.dob.age} <br/> <hr/>
+          Location: {data.location.country} <br/> <hr/>
+          Email: {data.email} <br/> 
+        </Card.Text>
+      </Card.Body>
+    </Card>
+     
+      </div>}
+    
+  </div>
+);
+};
